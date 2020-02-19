b5bfff762ef807da9c7b64123cfdd2d05cb5c87e
20171141 <20171141@IC3>
2020-02-19 14:35:41 +0600

    Add project files.

---
+++ Calculatemobilebill/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculatemobilebill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
